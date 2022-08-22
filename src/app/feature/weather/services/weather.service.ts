d90c2eccab6a1e0a7b0e2c70dd13373e0da4a053
Ogi62 <danguba962@gmail.com>
2022-08-22 21:05:06 +0200

    make service for geolocation for fetching the users location

--- src/app/feature/weather/services/weather.service.ts
+++ src/app/feature/weather/services/weather.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+interface CoordinatesInterface {
+  accuracy: number;
+  altitude: number | null;
+  altitudeAccuracy: number | null;
+  heading: number | null;
+  latitude: number;
+  longitude: number;
+  speed: number | null;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +17,16 @@ import { Injectable } from '@angular/core';
 export class WeatherService {
 
   constructor() { }
+
+  getCurrentLocation() {
+    return new Observable<CoordinatesInterface>((observer)=> {
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          observer.next(position.coords);
+          observer.complete();
+        },
+        (err) => observer.error(err)
+      )
+    })
+  }
 }
