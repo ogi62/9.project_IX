995d4e0368ec7c521376470178c9d9d8ecd3b88d
Ogi62 <danguba962@gmail.com>
2022-08-26 18:51:11 +0200

    changes for z-index and creating custom pipe

--- src/app/feature/news-api/news-api.module.ts
+++ src/app/feature/news-api/news-api.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewsApiListComponent } from './components/news-api-list/news-api-list.component';
+import { TrimOutletNamePipe } from './pipes/trim-outlet-name.pipe';
 
 
 
 @NgModule({
   declarations: [
-    NewsApiListComponent
+    NewsApiListComponent,
+    TrimOutletNamePipe
   ],
   imports: [
     CommonModule
